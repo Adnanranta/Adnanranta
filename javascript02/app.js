4045e28c037f94d74173fc904486543f75675cd0
Adnan Ranta <aadnanranta@gmail.com>
2023-04-12 00:13:39 +0500

    new fiel update

--- javascript02/app.js
+++ javascript02/app.js
@@ -16,6 +16,9 @@ let data3 = data + data2;  // strings Concatenation
 //let yourfirstfav =prompt("what is your favourate number");
 //console.log("wellcome to my websit" + yourfirstfav);
 //document.write("wellcome to my websit " + yourfirstfav);
+
+//input Number then generate table
+
 let numbertable =+prompt("Input table number");
 for (let i =1; i <= 10 ; i++)
 {
